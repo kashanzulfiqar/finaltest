bc0409172b7a52b671a1dcc6dc673fa4b4654d57
kashanzulfiqar <56619464+kashanzulfiqar@users.noreply.github.com>
2026-08-18 18:39:52 +0500

    Delete frontend directory

--- frontend/src/Entryfile/routerpath.jsx
+++
@@ -1,7 +0,0 @@
-const publicPath = '/blue/';
-
-export const routeCodes = {
-  HOME: publicPath,
-  LOGIN: `${ publicPath }login`,
-  REGISTER: `${ publicPath }register`,
-};
--- frontend/src/router_service/taskservice.jsx
+++
@@ -1,9 +0,0 @@
-
-import Tasks from '../MainPage/Employees/Projects/tasks/tasks.jsx';
-
-export default [  
-   {
-      path: 'tasks',
-      component: Tasks
-   }
-]
